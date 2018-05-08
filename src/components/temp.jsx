ff44853e14d07326b910848c976230593b6ece17
Hippolyte Jacomet <hippolyte.jacomet@finalcad.com>
2018-05-08 11:22:00 +0200

    woopsie

--- src/components/temp.jsx
+++
@@ -1,7 +0,0 @@
-<div>{popHoveredTree.fields.libellefrancais}</div>
-                <div>Espèce : {popHoveredTree.fields.espece}</div>
-                <div>Genre : {popHoveredTree.fields.genre}</div>
-                <div>Date de plantation : {popHoveredTree.fields.dateplantation}</div>
-<TreePopUp 
-                        hoveredTree={this.state.hoveredTree}
-                    />
