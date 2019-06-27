85bfe95875c1eb5935e5cc9cced207c71cc37f04
Hippolyte <hippolyte.jacomet@gmail.com>
2019-06-27 13:20:09 +0200

    Updates

---
+++ precache-manifest.c95efaa6e82e67aef57e48077319d0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0430b143c74e7084d24e7ab9e7477f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3c2b391b927aeb4883",
+    "url": "/static/css/2.0ed265e4.chunk.css"
+  },
+  {
+    "revision": "943d4353b99990959a54",
+    "url": "/static/css/main.84ff7520.chunk.css"
+  },
+  {
+    "revision": "ad3c2b391b927aeb4883",
+    "url": "/static/js/2.c568dd54.chunk.js"
+  },
+  {
+    "revision": "943d4353b99990959a54",
+    "url": "/static/js/main.cac9897a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
