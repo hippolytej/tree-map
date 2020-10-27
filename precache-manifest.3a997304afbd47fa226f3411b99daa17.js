9ef3d4aba837229cd0e0fd85eac1e8fa50407108
Hippolyte Jacomet <h.jacomet@instadeep.com>
2020-10-27 17:42:25 +0100

    Updates

---
+++ precache-manifest.3a997304afbd47fa226f3411b99daa17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5581061e5cff0eba5cf639056211adad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4fcd8fcb83a1e66e03b",
+    "url": "/static/css/2.0ed265e4.chunk.css"
+  },
+  {
+    "revision": "b1037b1e2d7bb7a471e0",
+    "url": "/static/css/main.478cffca.chunk.css"
+  },
+  {
+    "revision": "f4fcd8fcb83a1e66e03b",
+    "url": "/static/js/2.dae71ac1.chunk.js"
+  },
+  {
+    "revision": "b1037b1e2d7bb7a471e0",
+    "url": "/static/js/main.40dea14d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ba849151c9ea969ee8724dd45a90a15.js"
+  "/precache-manifest.3a997304afbd47fa226f3411b99daa17.js"
 );
 
 self.addEventListener('message', (event) => {
